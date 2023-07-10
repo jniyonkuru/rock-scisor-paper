26c2d9d796b1706b2c8a7ff5d017383481029936
jniyonkuru <niyonkurujacques@gmail.com>
2023-07-10 16:14:14 +0200

    Get the computer  random choice

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ let tieGames=0;
 const Rock="rock";
 const Paper="paper";
 const Scissor ="scissor";
+
+/*
+A FUNCTION THAT GENERATE A COMPUTER RANDOM CHOICE
+ 
+*/
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3) + 1;
+  switch (choice) {
+    case 1:
+      return Rock;
+      break;
+    case 2:
+      return Scissor;
+      break;
+    case 3:
+      return Paper;
+      break;
+  }
+}
